d4dda00b56f51109287cf22b5f6d816b4e41bd73
unknown <derbu@hotmail.com>
2019-02-06 11:52:06 -0200

    Page Admin

--- index.php
+++ index.php
@@ -12,12 +12,14 @@ $app = new Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-	$page = new Hcode\Page();
+	$page = new Page();
 
 	$page->setTpl("index");
 });
+
 $app->get('/admin', function() {
-	$page = new Hcode\PageAdmin();
+	
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 });
